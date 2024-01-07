8ac69536be1cedc9e084d34276aff244af5a5e4a
Ibrokhim3 <ishamsiddinov.97@gmail.com>
2024-01-07 20:26:00 +0500

    feat: components added

--- components/contact.tsx
+++ components/contact.tsx
@@ -29,15 +29,15 @@ export default function Contact() {
       }}
     >
       <SectionHeading>Contact me</SectionHeading>
-      <p className="text-gray-700 -mt-6">
-        Please contact me directly at
+      <p className="text-gray-700 -mt-6 dark:text-white/80">
+        Please contact me directly at{" "}
         <a className="underline" href="mailto:ishamsiddinov.97@gmail.com">
           ishamsiddinov.97@gmail.com
         </a>{" "}
         or through this form
       </p>
       <form
-        className="mt-10 flex flex-col"
+        className="mt-10 flex flex-col dark:text-black"
         action={async (formData) => {
           const { data, error } = await sendEmail(formData);
 
@@ -49,7 +49,7 @@ export default function Contact() {
         }}
       >
         <input
-          className="h-14 px-4 rounded-lg borderBlack"
+          className="h-14 px-4 rounded-lg borderBlack dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
           name="senderEmail"
           required
           maxLength={500}
@@ -57,7 +57,7 @@ export default function Contact() {
           type="email"
         />
         <textarea
-          className="h-52 my-3 rounded-lg borderBlack p-4"
+          className="h-52 my-3 rounded-lg borderBlack p-4 dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
           name="message"
           placeholder="Your message"
           required
